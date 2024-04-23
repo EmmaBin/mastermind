0667807026d929224c113001429bd1e5569b79a6
emmacodes <binma.emma@gmail.com>
2024-04-22 19:50:46 -0700

    displayed ten guesses

--- src/App.js
+++ src/App.js
@@ -7,7 +7,17 @@ function App() {
   const [currentRound, setCurrentRound] = React.useState(0)
   const [difficultyLevel, setLevel] = React.useState(4)
   const [formDetail, setFormDetail] = React.useState([])
-  function handleSubmit() {
+  // when submit -> 1. create a new form object in the format of { index: 0, value: [1, 2, 3] }
+  // 2. push to formDetail Array
+  // 3. check against secretCodes -> a new function needed here
+  // 4. display feedback
+  // 5. check game condition to determine if the game is still going
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    const newForm = new FormData(e.target)
+    console.log(...newForm.values())
+
 
   }
 
@@ -27,7 +37,8 @@ function App() {
     <div className="App">
       <h1>Mastermind Game ⏳</h1>
       <h2>{secretCodes}</h2>
-      <InputForm difficultyLevel={difficultyLevel} currentRound={currentRound} handleSubmit={handleSubmit} />
+      {Array.from({ length: 10 }, (_, index) => <InputForm key={index} difficultyLevel={difficultyLevel} currentRound={currentRound} handleSubmit={handleSubmit} index={index} />)}
+
 
 
 
--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,16 +1,15 @@
-export default function InputForm({ handleSubmit, currentRound, difficultyLevel }) {
+export default function InputForm({ index, handleSubmit, currentRound, difficultyLevel }) {
     //if current round is not equal to index, disable the input
     // const isDisabled = 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>Type Your Guess Here:</label>
-            {Array.from({ length: difficultyLevel }, (_, i) => <input type="number" min={0} max={7} id={i} required />)
+        <div >
+            <form onSubmit={handleSubmit}>
+                <label>{index + 1}. Type Your Guess Here: </label>
+                {Array.from({ length: difficultyLevel }, (_, i) => <input key={i} type="number" min={0} max={7} name={i} required />)
+                }
+                <button type="submit">Submit</button>
+            </form>
+        </div>
 
-            }
-
-
-
-            <button type="submit">Submit</button>
-        </form>
     )
 }
