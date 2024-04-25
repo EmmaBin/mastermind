8ea6128826d76106a8dc89ac36a20851075e1649
emmacodes <binma.emma@gmail.com>
2024-04-25 15:06:30 -0700

    styled timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import '../App.css';
 export default function Timer({ restart }) {
 
     const [second, setSecond] = React.useState(0)
@@ -16,9 +17,10 @@ export default function Timer({ restart }) {
     const minute = Math.trunc(second / 60)
 
     return (
-        <div>
 
+        <div className='timer'>
             {String(minute).padStart(2, '0')}:{second > 60 ? String(second % 60).padStart(2, '0') : String(second).padStart(2, '0')}
         </div>
+
     )
 }
