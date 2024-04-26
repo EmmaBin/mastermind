66ed8d0c07c896863a3b64bfea5a85669feebc22
emmacodes <binma.emma@gmail.com>
2024-04-25 19:38:41 -0700

    added more testing cases for getCodes function

--- src/tests/getCodes.test.js
+++ src/tests/getCodes.test.js
@@ -15,6 +15,33 @@ describe("fetch codes from API", () => {
         expect(fetch).toHaveBeenCalledWith(`https://www.random.org/integers/?num=${counts}&min=0&max=7&col=1&base=10&format=plain&rnd=new`);
     })
 
+    it("should get codes for medium level", async () => {
+        const counts = 5
+        const mockData = "12345\n45675\n78905\n12345"
+        global.fetch = jest.fn().mockResolvedValueOnce({
+            text: jest.fn().mockResolvedValueOnce(mockData)
+        })
+
+        const result = await getCodes(counts)
+
+        expect(result).toEqual(['12345', '45675', '78905', '12345'])
+        expect(fetch).toHaveBeenCalledTimes(1)
+        expect(fetch).toHaveBeenCalledWith(`https://www.random.org/integers/?num=${counts}&min=0&max=7&col=1&base=10&format=plain&rnd=new`);
+    })
+
+    it("should get codes for hard level", async () => {
+        const counts = 6
+        const mockData = "123466\n456766\n789066\n123466"
+        global.fetch = jest.fn().mockResolvedValueOnce({
+            text: jest.fn().mockResolvedValueOnce(mockData)
+        })
+
+        const result = await getCodes(counts)
+
+        expect(result).toEqual(['123466', '456766', '789066', '123466'])
+        expect(fetch).toHaveBeenCalledTimes(1)
+        expect(fetch).toHaveBeenCalledWith(`https://www.random.org/integers/?num=${counts}&min=0&max=7&col=1&base=10&format=plain&rnd=new`);
+    })
     it("should return null to handle error", async () => {
         global.fetch = jest.fn().mockResolvedValueOnce("API Error")
 
